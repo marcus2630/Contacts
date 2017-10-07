5083b9e8974dc4d4d5b60247660ee53db8ed78f8
Marcus Klausen <marcusklausen@gmail.com>
2017-10-07 17:24:32 +0200

    Initial Commit

---
+++ Contacts/ContactListController.swift
@@ -0,0 +1,34 @@
+//
+//  ContactListController.swift
+//  Contacts
+//
+//  Created by Marcus Klausen on 07/10/2017.
+//  Copyright © 2017 Marcus Klausen. All rights reserved.
+//
+
+import UIKit
+
+class ContactListController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
